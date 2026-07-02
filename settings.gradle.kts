5899d4dd0dea05f8dc0f372268e7ba56c29e6e59
Beya Lora Ramen <beya.lora.ramen@gmail.com>
2026-07-02 23:52:25 +0530

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode = RepositoriesMode.FAIL_ON_PROJECT_REPOS
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Apps"
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+include(":app")
